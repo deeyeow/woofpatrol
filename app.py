1aafb0e037f6a853b2e4972557bdc98dcb62d5d7
Darren Yau <mrdarrenyau@gmail.com>
2019-11-14 01:50:06 -0800

    Init app

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for, request, redirect
+from pymongo import MongoClient
+
+from filter import videoCapture
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        videoCapture()
+        return redirect('/')
+    else:
+        render_template('index.html')
+
+    return render_template('index.html')
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
