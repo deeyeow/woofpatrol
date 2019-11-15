a461220e8f125186118be2fd56ae54c13b6474ea
Darren Yau <mrdarrenyau@gmail.com>
2019-11-15 01:06:15 -0800

    removed live webcam, added upload system

--- app.py
+++ app.py
@@ -1,24 +1,76 @@
 from flask import Flask, render_template, url_for, request, redirect
 from pymongo import MongoClient
 
-from filter import videoCapture
+from filter import apply_mask
 
 import cv2
 import numpy as np
+import base64
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        videoCapture()
-        return redirect('/')
-    else:
-        render_template('index.html')
+    # if request.method == 'POST':
+    #     videoCapture()
+    #     return redirect('/')
+    # else:
+    #     render_template('index.html')
 
     return render_template('index.html')
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['image']
 
+    # Save file
+    #filename = 'static/' + file.filename
+    #file.save(filename)
+
+    # Read image
+    image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
+    # print(image.shape)
+    
+    # load mask
+    mask = cv2.imread('assets/snapchat_dog.png')
+    
+    # initialize front face classifier
+    cascade = cv2.CascadeClassifier("assets/haarcascade_frontalface_default.xml")
+   
+    # Get image shape
+    image_h, image_w, _ = image.shape
+
+    # Convert to black-and-white, and add contrast
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blackwhite = cv2.equalizeHist(gray)
+
+    # Detect faces
+    rects = cascade.detectMultiScale(
+        blackwhite, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE)
+
+    # Add all bounding boxes to the image
+    for x, y, w, h in rects:
+        # crop a frame slightly larger than the face
+        y0, y1 = int(y - 0.20*h), int(y + 0.80*h)
+        x0, x1 = x, x + w
+
+        # give up if the cropped frame would be out-of-bounds
+        if x0 < 0 or y0 < 0 or x1 > image_w or y1 > image_h:
+            continue
+
+        # apply mask
+        image[y0: y1, x0: x1] = apply_mask(image[y0: y1, x0: x1], mask)
+
+    # Save
+    #cv2.imwrite(filename, image)
+        
+    # In memory
+    image_content = cv2.imencode('.jpg', image)[1].tostring()
+    encoded_image = base64.encodebytes(image_content)
+    to_send = 'data:image/jpg;base64, ' + str(encoded_image, 'utf-8')
+
+    return render_template('index.html', image_to_show=to_send)
 
 
 
--- filter.py
+++ filter.py
@@ -18,59 +18,60 @@ def apply_mask(face: np.array, mask: np.array) -> np.array:
     # Add mask to face - ensure mask is centered
     resized_mask = cv2.resize(mask, new_mask_shape)
     face_with_mask = face.copy()
-    non_white_pixels = (resized_mask < 250).all(axis=2)
+    non_white_pixels = (resized_mask > 0).all(axis=2)
 
     off_h = int((face_h - new_mask_h) / 2)  
     off_w = int((face_w - new_mask_w) / 2)
 
     face_with_mask[off_h: off_h+new_mask_h, off_w: off_w+new_mask_w][non_white_pixels] = resized_mask[non_white_pixels] #numpy indexing by mask
 
+
     return face_with_mask
 
-def videoCapture():
-    cap = cv2.VideoCapture(0)
+# def render_image():
+#     cap = cv2.VideoCapture(0)
 
-    # load mask
-    mask = cv2.imread('assets/dog.png')
+#     # load mask
+#     mask = cv2.imread('assets/dog.png')
         
-    # initialize front face classifier
-    cascade = cv2.CascadeClassifier("assets/haarcascade_frontalface_default.xml")
+#     # initialize front face classifier
+#     cascade = cv2.CascadeClassifier("assets/haarcascade_frontalface_default.xml")
     
-    while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        frame_h, frame_w, _ = frame.shape
+#     while True:
+#         # Capture frame-by-frame
+#         ret, frame = cap.read()
+#         frame_h, frame_w, _ = frame.shape
 
-        # Convert to black-and-white, and add contrast
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blackwhite = cv2.equalizeHist(gray)
+#         # Convert to black-and-white, and add contrast
+#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#         blackwhite = cv2.equalizeHist(gray)
 
-        # Detect faces
-        rects = cascade.detectMultiScale(
-            blackwhite, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE)
+#         # Detect faces
+#         rects = cascade.detectMultiScale(
+#             blackwhite, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
+#             flags=cv2.CASCADE_SCALE_IMAGE)
 
-        # Add all bounding boxes to the image
-        for x, y, w, h in rects:
-            # crop a frame slightly larger than the face
-            y0, y1 = int(y - 0.25*h), int(y + 0.75*h)
-            x0, x1 = x, x + w
+#         # Add all bounding boxes to the image
+#         for x, y, w, h in rects:
+#             # crop a frame slightly larger than the face
+#             y0, y1 = int(y - 0.25*h), int(y + 0.75*h)
+#             x0, x1 = x, x + w
 
-            # give up if the cropped frame would be out-of-bounds
-            if x0 < 0 or y0 < 0 or x1 > frame_w or y1 > frame_h:
-                continue
+#             # give up if the cropped frame would be out-of-bounds
+#             if x0 < 0 or y0 < 0 or x1 > frame_w or y1 > frame_h:
+#                 continue
 
-            # apply mask
-            frame[y0: y1, x0: x1] = apply_mask(frame[y0: y1, x0: x1], mask)
+#             # apply mask
+#             frame[y0: y1, x0: x1] = apply_mask(frame[y0: y1, x0: x1], mask)
         
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+#         # Display the resulting frame
+#         cv2.imshow('frame', frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
 
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+#     # When everything done, release the capture
+#     cap.release()
+#     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
-    videoCapture()
+# if __name__ == '__main__':
+#     render_image()
